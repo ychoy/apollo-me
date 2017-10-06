7175e4a685d4687a85134a9ae5b50a89d5066574
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-06 11:08:55 -0700

    define entry points to API and specify object types

--- src/schema.js
+++ src/schema.js
@@ -1,12 +1,19 @@
-export const typeDefs = `
+import {
+  makeExecutableSchema,
+  addMockFunctionsToSchema, 
+} from 'graphql-tools';
+
+const typeDefs = `
 type Channel {
    id: ID!                # "!" denotes a required field
    name: String
 }
 # This type specifies the entry points into our API. In this case
-# there is only one - "channel" - which returns a list of channels.
-
+# there is only one - "channels" - which returns a list of channels.
 type Query {
-   channels: [Channel]    # "[]" means this is a list of albums
+   channels: [Channel]    # "[]" means this is a list of channels
 }
 `;
+
+const schema = makeExecutableSchema({ typeDefs });
+export { schema };
