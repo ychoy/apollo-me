ffed73897dd385d9f17e564a3a59b3cca4167020
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-06 11:16:13 -0700

    generate server schema

---
+++ server/src/schema.js
@@ -0,0 +1,19 @@
+import {
+  makeExecutableSchema,
+  addMockFunctionsToSchema, 
+} from 'graphql-tools';
+
+const typeDefs = `
+type Channel {
+   id: ID!                # "!" denotes a required field
+   name: String
+}
+# This type specifies the entry points into our API. In this case
+# there is only one - "channels" - which returns a list of channels.
+type Query {
+   channels: [Channel]    # "[]" means this is a list of channels
+}
+`;
+
+const schema = makeExecutableSchema({ typeDefs });
+export { schema };
