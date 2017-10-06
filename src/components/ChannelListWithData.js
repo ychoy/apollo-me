80b95ccca4aabca4dc5ce9ec7f0e456467620db5
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-05 17:06:11 -0700

    generate ChannelList component

---
+++ src/components/ChannelListWithData.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  Link
+} from 'react-router-dom'
+
+import {
+    gql,
+    graphql,
+} from 'react-apollo';
+
+const ChannelsList = ({ data: {loading, error, channels }}) => {
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
+   return <ul>
+     { channels.map( ch => <li key={ch.id}>{ch.name}</li> ) }
+   </ul>;
+};
+
+export const channelsListQuery = gql`
+  query ChannelsListQuery {
+    channels {
+      id
+      name
+    }
+  }
+`;
+
+export default graphql(channelsListQuery, {
+  options: { pollInterval: 5000 },
+})(ChannelsList);
