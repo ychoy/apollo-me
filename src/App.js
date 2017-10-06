914c1552d6a936105a8c4e5319a31b24850a35d3
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-06 11:01:57 -0700

    include mock network into apollo client

--- src/App.js
+++ src/App.js
@@ -1,39 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {
-  ApolloClient,
-  gql,
-  graphql,
-  ApolloProvider,
-} from 'react-apollo';
 
-import ChannelsListWithData from './components/ChannelsListWithData';
+import ApolloClient from 'apollo-client';
+import { graphql, ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 
-const client = new ApolloClient();
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import { mockNetworkInterfaceWithSchema } from 'apollo-test-utils';
+import { typeDefs } from './schema';
 
-const ChannelssList = () =>
-     (<ul>
-       <li>Channels 1</li>
-       <li>Channels 2</li>
-     </ul>);
+const schema = makeExecutableSchema({ typeDefs });
+addMockFunctionsToSchema({ schema });
 
-// Wrap component with ApolloProvider to put  an instance of the client on the UI.
+const mockNetworkInterface = mockNetworkInterfaceWithSchema({ schema });
+
+// pass mockNetworkInterface into Apollo Client constructor 
+const client = new ApolloClient({
+  networkInterface: mockNetworkInterface,
+});
+
+const ChannelsList = ({ data: {loading, error, channels }}) => {
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
+  return <ul>
+    { channels.map( ch => <li key={ch.id}>{ch.name}</li> ) }
+  </ul>;
+};
+
+const channelsListQuery = gql`
+  query ChannelsListQuery {
+    channels {
+      id
+      name
+    }
+  }
+`;
+
+const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
 
 class App extends Component {
-   render() {
-     return (
-       <ApolloProvider client={client}>
-         <div className="App">
-           <div className="App-header">
-             <img src={logo} className="App-logo" alt="logo" />
-             <h2>Apollo Me</h2>
-           </div>
-           <ChannelsListWithData />
-         </div>
-       </ApolloProvider>
-     );
-   }
- }
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Apollo Me</h2>
+          </div>
+          <ChannelsListWithData />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
 
 export default App;
