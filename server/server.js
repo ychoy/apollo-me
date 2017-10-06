cbaafef3b4fe0c5413f78c50d1753496f11ab0f4
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-06 16:27:41 -0700

    tell Express to pass requests to graphqlExpress middleware

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import { 
+  graphqlExpress,
+  graphiqlExpress,
+} from 'graphql-server-express';
+import bodyParser from 'body-parser';
+import { schema } from './src/schema';
 
 const PORT = 4000;
 
 const server = express();
 
-server.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+server.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema
+}));
 
 server.listen(PORT, () => console.log(`GraphQL Server is now running on http://localhost:${PORT}`));
