ac940b4d55a5a611b912ded329a956958d5f71ef
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-09 16:11:51 -0700

    add AddChannel component

--- client/src/components/AddChannel.js
+++ client/src/components/AddChannel.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { gql, graphql } from 'react-apollo';
-import { channelsListQuery } from './ChannelsListWithData';
 
-/*
-  The mutation returns a channel object, which we can then select fields on, just as with a query. 
-  The handleKeyUp function Prints input text to console and clears input field when user hits return (key code 13).
-  Wire component up with GraphQL higher order component (HOC) from react-apollo.
-  Call the mutate prop of HOC to execute mutation 
-  */
+import { channelsListQuery } from './ChannelsListWithData';
 
 const AddChannel = ({ mutate }) => {
   const handleKeyUp = (evt) => {
     if (evt.keyCode === 13) {
-      evt.persist();
-      mutate({ /* Tell Apollo Client we want to refetch channels after our mutation completes */ 
-        variables: { name: evt.target.value }, 
-        refetchQueries: [ { query: channelsListQuery }], /*Pass it via refectchQueries option to the call to mutate. Then export query from ChannelsListWithData and import it into AddChannel. This allows new channels to update instantly after refetching list of channels */
-      })
-      .then( res => {
-        evt.target.value = '';
+      mutate({ 
+        variables: { name: evt.target.value },
+        optimisticResponse: {
+          addChannel: {
+            name: evt.target.value,
+            id: Math.round(Math.random() * -1000000),
+            __typename: 'Channel',
+          },
+        },
+        update: (store, { data: { addChannel } }) => {
+            // Read the data from the cache for this query.
+            const data = store.readQuery({ query: channelsListQuery });
+            // Add our channel from the mutation to the end.
+            data.channels.push(addChannel);
+            // Write the data back to the cache.
+            store.writeQuery({ query: channelsListQuery, data });
+          },
       });
+      evt.target.value = '';
     }
   };
 
