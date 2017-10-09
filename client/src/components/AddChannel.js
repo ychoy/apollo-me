bdedc3f9b5089ad2d8a1b1a362e8ab759494ba8f
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-09 14:09:41 -0700

    call the muitation from AddChannel component'

--- client/src/components/AddChannel.js
+++ client/src/components/AddChannel.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { gql, graphql } from 'react-apollo';
+import { channelsListQuery } from './ChannelsListWithData';
 
-/* 
-  handleKeyUp function Prints input text to console and clears input field when user hits return (key code 13)
- */
-const AddChannel = () => {
+/*
+  The mutation returns a channel object, which we can then select fields on, just as with a query. 
+  The handleKeyUp function Prints input text to console and clears input field when user hits return (key code 13).
+  Wire component up with GraphQL higher order component (HOC) from react-apollo.
+  Call the mutate prop of HOC to execute mutation 
+  */
+
+const AddChannel = ({ mutate }) => {
   const handleKeyUp = (evt) => {
     if (evt.keyCode === 13) {
-      console.log(evt.target.value);
-      evt.target.value = '';
+      evt.persist();
+      mutate({
+        variables: { name: evt.target.value },
+        refetchQueries: [ { query: channelsListQuery }],
+      })
+      .then( res => {
+        evt.target.value = '';
+      });
     }
   };
+
   return (
     <input
       type="text"
@@ -18,4 +31,19 @@ const AddChannel = () => {
     />
   );
 };
-export default AddChannel;
+
+const addChannelMutation = gql`
+  mutation addChannel($name: String!) {
+    addChannel(name: $name) {
+      id
+      name
+    }
+  }
+`;
+
+
+const AddChannelWithMutation = graphql(
+  addChannelMutation,
+)(AddChannel);
+
+export default AddChannelWithMutation;
