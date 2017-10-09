0c5919772e0b8bb963fde789cdd7e29da5454682
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-09 13:59:18 -0700

    define resolver function for mutation

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,15 +1,26 @@
-const channels = [{
+const channels = 
+/*[{
   id: 1,
   name: 'libraries',
 }, {
   id: 2,
   name: 'parks',
 }];
+*/
+
+let nextId = 3;
 
 export const resolvers = {
   Query: {
     channels: () => {
       return channels;
+    },
+  },
+  Mutation: {
+    addChannel: (root, args) => {
+      const newChannel = { id: nextId++, name: args.name };
+      channels.push(newChannel);
+      return newChannel;
     },
   },
 };
